b3124622baed55e5994f134a2f4d5bf752bed75a
walid.sadou <walid.sadou@ibm.com>
2020-05-15 10:56:27 +0200

    All test suite for classic roman number

--- src/main/java/com/epita/socra/app/tools/RomanConverter.java
+++ src/main/java/com/epita/socra/app/tools/RomanConverter.java
@@ -3,7 +3,9 @@ package com.epita.socra.app.tools;
 public class RomanConverter {
 
     public String convertToRoman(int number) {
-        return "V";
+        String result = "V";
+        return result;
     }
+
 }
 
--- src/test/java/com/epita/socra/app/tools/RomanConverterTest.java
+++ src/test/java/com/epita/socra/app/tools/RomanConverterTest.java
@@ -1,13 +1,46 @@
 package com.epita.socra.app.tools;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class RomanConverterTest {
+    @Test
+    public void given1_ThenConvertToI() {
+        RomanConverter mock = mock(RomanConverter.class);
+        assertEquals("I", mock.convertToRoman(1));
+    }
 
+    @Test
     public void given5_ThenConvertToV() {
         RomanConverter mock = mock(RomanConverter.class);
-        assertEquals(mock.convertToRoman(5), "V");
+        assertEquals("V", mock.convertToRoman(5));
+    }
+    @Test
+    public void given10_ThenConvertToX() {
+        RomanConverter mock = mock(RomanConverter.class);
+        assertEquals("X", mock.convertToRoman(10));
+    }
+    @Test
+    public void given50_ThenConvertToL() {
+        RomanConverter mock = mock(RomanConverter.class);
+        assertEquals("L", mock.convertToRoman(50) );
+    }
+    @Test
+    public void given100_ThenConvertToC() {
+        RomanConverter mock = mock(RomanConverter.class);
+        assertEquals("C", mock.convertToRoman(100));
+    }
+    @Test
+    public void given500_ThenConvertToD() {
+        RomanConverter mock = mock(RomanConverter.class);
+        assertEquals("D", mock.convertToRoman(500));
+    }
+    @Test
+    public void given1000_ThenConvertToM() {
+        RomanConverter mock = mock(RomanConverter.class);
+        assertEquals("M", mock.convertToRoman(1000));
     }
 }
